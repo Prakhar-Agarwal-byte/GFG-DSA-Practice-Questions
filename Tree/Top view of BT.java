016968a17f07e8224eebe8cd96040b76bea88078
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-21 12:21:19 +0530

    Create Top view of BT.java

---
+++ Tree/Top view of BT.java
@@ -0,0 +1,5 @@
+// Same as https://github.com/Prakhar-Agarwal-byte/Tree-Questions-GFG/blob/main/Vertical%20order%20traversal
+
+// Just pick the first element from each arraylist and that will be the top view
+
+// For bottom view pick the last element from each arraylist 
