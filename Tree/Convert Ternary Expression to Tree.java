1e09570de8318c7e36809881a3d6848e8972efec
Prakhar Agarwal <prakhar@everything.net.in>
2021-07-23 20:18:22 +0530

    Create Convert Ternary Expression to Tree.java

---
+++ Tree/Convert Ternary Expression to Tree.java
@@ -0,0 +1,12 @@
+class GfG {
+  public static Node convertExp(String str, int i) {
+    if (i > str.length())
+      return null;
+    Node root = new Node(str.charAt(i++));
+    if (i < str.length() && str.charAt(i) == '?')
+      root.left = convertExp(str, i + 1);
+    if (i < str.length() && str.charAt(i) == ':')
+      root.right = convertExp(str, i + 1);
+    return root;
+  }
+}
