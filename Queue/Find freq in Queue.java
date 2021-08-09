d99c43f207424d0a31cf62361b0a6d5b7c11cd61
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-09 09:35:49 +0530

    Create Find freq in Queue.java

---
+++ Queue/Find freq in Queue.java
@@ -0,0 +1,16 @@
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks {
+
+  // Function to insert element into the queue
+  static void insert(Queue<Integer> q, int k) {
+    q.add(k);
+  }
+
+  // Function to find frequency of an element
+  // rteturn the frequency of k
+  static int findFrequency(Queue<Integer> q, int k) {
+    return Collections.frequency(q, k);
+  }
+
+}
