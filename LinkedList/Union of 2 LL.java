c04febf9af8f605b63083730bb8c5f09d4247c1a
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-20 16:41:24 +0530

    Create Union of 2 LL.java

---
+++ LinkedList/Union of 2 LL.java
@@ -0,0 +1,2 @@
+// Basically either use hashmap and store visited elements or sort the array and merge 2 sorted lists
+//https://practice.geeksforgeeks.org/problems/union-of-two-linked-list/1/?category[]=Linked%20List&category[]=Linked%20List&page=2&query=category[]Linked%20Listpage2category[]Linked%20List
