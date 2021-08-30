6f8e8245dab942c5cc50b6d18d98d5e3772af864
Prakhar Agarwal <prakhar@everything.net.in>
2021-08-30 17:09:03 +0530

    Update Egg Drop.java

--- DP/Egg Drop.java
+++ DP/Egg Drop.java
@@ -1,3 +1,4 @@
+// Memoized
 import java.util.Arrays;
 
 class Solution {
@@ -23,5 +24,30 @@ class Solution {
       min = Math.min(min, temp);
     }
     return dp[k][n] = min;
+  }
+}
+
+// Tabulated
+class Solution {
+  static int eggDrop(int n, int k) {
+    int[][] dp = new int[n + 1][k + 1];
+    for (int i = 0; i <= n; i++) {
+      dp[i][1] = 1;
+    }
+    for (int i = 1; i <= k; i++) {
+      dp[0][i] = Integer.MIN_VALUE;
+      dp[1][i] = i;
+    }
+    for (int i = 2; i <= n; i++) {
+      for (int j = 2; j <= k; j++) {
+        int min = Integer.MAX_VALUE;
+        for (int a = 1; a <= j; a++) {
+          int temp = 1 + Math.max(dp[i][j - a], dp[i - 1][a - 1]);
+          min = Math.min(min, temp);
+          dp[i][j] = min;
+        }
+      }
+    }
+    return dp[n][k];
   }
 }
