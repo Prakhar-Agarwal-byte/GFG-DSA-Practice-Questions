0728aabd88647674175b77ee7b3c38d6df9b249d
Prakhar Agarwal <prakhar@everything.net.in>
2021-09-07 16:39:43 +0530

    Create Largest rectanglw with equal 1 and 0.java

---
+++ DP/Largest rectanglw with equal 1 and 0.java
@@ -0,0 +1,2 @@
+// Same as Largest reactangle with 0 sum https://github.com/Prakhar-Agarwal-byte/GFG-DSA-Practice-Questions/blob/main/DP/Largest%20rectangle%20with%200%20sum.java
+// Just treat 0 as 1 and 1 as -1.
