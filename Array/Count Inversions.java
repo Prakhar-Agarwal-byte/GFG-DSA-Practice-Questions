01a28e72ab5f7b099f45c05f8470ae2fef0c0fc2
Prakhar Agarwal <prakhar@everything.net.in>
2021-09-14 10:54:08 +0530

    Update Count Inversions.java

--- Array/Count Inversions.java
+++ Array/Count Inversions.java
@@ -2,37 +2,39 @@ import java.util.Arrays;
 
 class Solution {
   static long inversionCount(long arr[], long N) {
-    return mergeCount(arr, 0, arr.length - 1);
+    return mergeCount(arr, 0, (int) N - 1);
   }
 
-  static long mergeCount(long arr[], int left, int right) {
+  private static long mergeCount(long arr[], int start, int end) {
     long count = 0;
-    while (left < right) {
-      int mid = left + (right - left) / 2;
-      count += mergeCount(arr, left, mid - 1);
-      count += mergeCount(arr, mid + 1, right);
-      count += merge(arr, left, mid, right);
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      count += mergeCount(arr, start, mid);
+      count += mergeCount(arr, mid + 1, end);
+      count += merge(arr, start, mid, end);
     }
     return count;
   }
 
-  static long merge(long arr[], int left, int mid, int right) {
-    long[] leftArr = Arrays.copyOfRange(arr, left, mid - 1);
-    long[] rightArr = Arrays.copyOfRange(arr, mid + 1, right);
-    int i = 0, j = 0, k = left, swaps = 0;
+  private static long merge(long[] arr, int start, int mid, int end) {
+    long[] leftArr = Arrays.copyOfRange(arr, start, mid + 1);
+    long[] rightArr = Arrays.copyOfRange(arr, mid + 1, end);
+    long swaps = 0;
+    int i = 0, j = 0, k = start;
     while (i < leftArr.length && j < rightArr.length) {
       if (leftArr[i] <= rightArr[j]) {
         arr[k++] = leftArr[i++];
       } else {
         arr[k++] = rightArr[j++];
-        swaps += mid - i;
+        swaps += leftArr.length - i;
       }
     }
-    while (i < leftArr.length)
+    while (i < leftArr.length) {
       arr[k++] = leftArr[i++];
-    while (j < rightArr.length)
+    }
+    while (j < rightArr.length) {
       arr[k++] = rightArr[j++];
+    }
     return swaps;
-
   }
 }
