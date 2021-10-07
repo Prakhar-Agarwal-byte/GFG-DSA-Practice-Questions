0b5b91b133de4e382ff216dca7b97c049680faea
Prakhar Agarwal <prakhar@everything.net.in>
2021-10-07 10:31:32 +0530

    Update DFS graph.java

--- Graph/DFS graph.java
+++ Graph/DFS graph.java
@@ -1,5 +1,4 @@
-import java.util.*;
-
+// DFS recursive
 class Solution {
   // Function to return a list containing the DFS traversal of the graph.
   public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -15,6 +14,32 @@ class Solution {
     for (int e : adj.get(i)) {
       if (!visited[e]) {
         dfs(e, list, visited, adj);
+      }
+    }
+  }
+}
+// DFS iterative
+class Solution {
+  // Function to return a list containing the DFS traversal of the graph.
+  public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    boolean[] visited = new boolean[V];
+    ArrayList<Integer> list = new ArrayList<>();
+    dfs(0, list, visited, new Stack<Integer>(), adj);
+    return list;
+  }
+
+  private void dfs(int i, ArrayList<Integer> list, boolean[] visited, Stack<Integer> st,
+      ArrayList<ArrayList<Integer>> adj) {
+    st.push(i);
+    visited[i] = true;
+    while (!st.isEmpty()) {
+      int cur = st.pop();
+      list.add(cur);
+      for (int e : adj.get(cur)) {
+        if (!visited[e]) {
+          visited[e] = true;
+          st.push(e);
+        }
       }
     }
   }
