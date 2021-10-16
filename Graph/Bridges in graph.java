1512d4d11cc584452e74e1a77554ea6b7512154a
Prakhar Agarwal <prakhar@everything.net.in>
2021-10-16 18:39:34 +0530

    Update Bridges in graph.java

--- Graph/Bridges in graph.java
+++ Graph/Bridges in graph.java
@@ -7,31 +7,27 @@ class Solution {
     boolean[] visited = new boolean[V];
     int[] disc = new int[V];
     int[] low = new int[V];
-    int[] parent = new int[V];
-    Arrays.fill(parent, -1);
     for (int i = 0; i < V; i++) {
       for (int e : adjList.get(i)) {
         if (!visited[e]) {
-          dfs(e, visited, disc, low, parent, adjList);
+          dfs(e, visited, disc, low, -1, adjList);
         }
       }
 
     }
   }
 
-  private void dfs(int i, boolean[] visited, int[] disc, int[] low, int[] parent,
-      ArrayList<ArrayList<Integer>> adjList) {
+  private void dfs(int i, boolean[] visited, int[] disc, int[] low, int parent, ArrayList<ArrayList<Integer>> adjList) {
     visited[i] = true;
     disc[i] = low[i] = timer++;
     for (int e : adjList.get(i)) {
       if (!visited[e]) {
-        parent[e] = i;
-        dfs(e, visited, disc, low, parent, adjList);
+        dfs(e, visited, disc, low, i, adjList);
         low[i] = Math.min(low[i], low[e]);
         if (low[e] > disc[i]) {
           System.out.println(i + "-" + e);
         }
-      } else if (parent[e] != i) {
+      } else if (e != parent) {
         low[i] = Math.min(low[i], disc[e]);
       }
     }
